e68dcaf1bf3728a7c99c0d91174e78b6513f088a
gabriele-kaskeviciute <gabi.kaskeviciute@gmail.com>
2023-11-20 19:44:26 +0200

    last update

--- script.js
+++ script.js
@@ -29,24 +29,52 @@
 // Išsaugokite įvestus skaičius į skaiciuMasyvas
 // Atspausdinkite tokį rezultatą:
 
+
 // Skaičių suma: x
 // Skaičių vidurkis: x
 // Didžiausias skaičius: x
 // Mažiausias skaičius: x
+
+
 // P.S.
 // Kad surasti sumą - panaudokite for / while ciklą. To pačio ciklo viduje taip pat galite ieškoti didžiausio/mažiausio skaičiaus ir jį saugoti.
 
 
-var skaiciuMasyvas = [23, 12, 564, 78, 10]
 
-var sum =0;
+//=========== Skaičių suma: x ===========
+// var skaiciuMasyvas = [23, 12, 564, 78, 10]
+
+// var sum =0;
+
+// skaiciuMasyvas.forEach( num => {
+//   sum += num;
+// })
+
+// console.log(sum)
+
+//=========== Skaičių vidurkis: x ===========
+
+// var skaiciuMasyvas = [23, 12, 564, 78, 10]
+
+// const sum = skaiciuMasyvas. reduce((a, b) => a + b, 0); 
+
+// const average = (sum / skaiciuMasyvas. length) || 0;
+
+// console.log(average)
 
 
+//=========== Didžiausias skaičius: x ===========
 
-skaiciuMasyvas.forEach( num => {
-  sum += num;
-})
+// var skaiciuMasyvas = [23, 12, 564, 78, 10]
 
-console.log(sum)
+// const max = Math.max(...skaiciuMasyvas);
 
+// console.log(max)
 
+//=========== Mažiausias skaičius: x ===========
+
+// var skaiciuMasyvas = [23, 12, 564, 78, 10]
+
+// const min = Math.min(...skaiciuMasyvas);
+
+// console.log(min)
